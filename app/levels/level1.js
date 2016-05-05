56d375ee2a86d811f8e4bc6c0a92f100568329b0
sloschi <seb.losch@gmail.com>
2016-05-05 21:24:27 +0200

    fixed idle animation

--- app/levels/level1.js
+++ app/levels/level1.js
@@ -1,4 +1,4 @@
-Game.Level1 = function (aGame) {};
+Game.Level1 = function (aGame) { };
 
 var map;
 var layer;
@@ -14,7 +14,7 @@ Game.Level1.prototype = {
 
         this.physics.arcade.gravity.y = 1400;
 
-        map =  this.add.tilemap('map', 64,64);
+        map = this.add.tilemap('map', 64, 64);
 
         map.addTilesetImage('maptileset');
 
@@ -22,14 +22,14 @@ Game.Level1.prototype = {
 
         layer.resizeWorld();
 
-        map.setCollisionBetween(0,0);
+        map.setCollisionBetween(0, 0);
 
-        player = this.add.sprite(100,560, 'player');
-        player.anchor.setTo(0.5,0.5);
+        player = this.add.sprite(100, 560, 'player');
+        player.anchor.setTo(0.5, 0.5);
 
-        player.animations.add('idle', [0,1], 1,true);
-        player.animations.add('jump', [2], 1,true);
-        player.animations.add('run', [3,4,5,6,7,8], 7,true);
+        player.animations.add('idle', [0, 1], 1, true);
+        player.animations.add('jump', [2], 1, true);
+        player.animations.add('run', [3, 4, 5, 6, 7, 8], 7, true);
         this.physics.arcade.enable(player);
         this.camera.follow(player);
         player.body.collideWorldBounds = true;
@@ -39,30 +39,28 @@ Game.Level1.prototype = {
             left: this.input.keyboard.addKey(Phaser.Keyboard.A),
             up: this.input.keyboard.addKey(Phaser.Keyboard.W)
         };
-    }, 
-    
+    },
+
     update: function () {
         this.physics.arcade.collide(player, layer);
 
         player.body.velocity.x = 0;
 
-        if(controls.up.isDown && (player.body.onFloor() || player.body.touching.down) && this.time.now > jumpTimer) {
+        if (controls.up.isDown && (player.body.onFloor() || player.body.touching.down) && this.time.now > jumpTimer) {
             player.animations.play('jump');
-
             player.body.velocity.y = -600;
             jumpTimer = this.time.now + 750;
-        }
-
-        if(controls.right.isDown) {
-            player.animations.play('run');
-            player.scale.setTo(1, 1);
-            player.body.velocity.x -= playerSpeed;
-        }
-
-        if(controls.left.isDown) {
+        } else if (controls.right.isDown) {
             player.animations.play('run');
             player.scale.setTo(1, 1);
             player.body.velocity.x += playerSpeed;
+        } else if (controls.left.isDown) {
+            player.animations.play('run');
+            player.scale.setTo(1, 1);
+            player.body.velocity.x -= playerSpeed;
+        } else {
+            player.animations.play('idle');
+            player.scale.setTo(1, 1);
         }
     }
 };
